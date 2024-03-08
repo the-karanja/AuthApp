e35e6392f094bdf7bbbe5fb3ac1aa69b76321e19
cafeway <bcaranja@gmail.com>
2024-03-08 20:24:38 +0300

    the routes '/login' and 'register' point to their index methods of the authenticator controllers to render their get requests

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Authenticator;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',[Authenticator::class, 'LoginIndex']);
+Route::get('/register',[Authenticator::class, 'RegisterIndex']);
