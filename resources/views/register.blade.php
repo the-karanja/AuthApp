9c6c88754b1db65511bbb383f41dd8baaa5f6583
cafeway <bcaranja@gmail.com>
2024-03-08 20:39:34 +0300

    resolved conflicting input field id's

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -14,7 +14,7 @@
                 <form>
                     <div class="mb-3">
                         <label for="email" class="form-label">Username</label>
-                        <input type="email" class="form-control" id="email" name="email" required placeholder="Username">
+                        <input type="email" class="form-control" id="username" name="username" required placeholder="Username">
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
@@ -24,15 +24,12 @@
                         <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" name="password" required>
                     </div>
-                    <div class="mb-3">
-                        <label for="confirm-password" class="form-label"> Confirm Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                    </div>
+
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="remember" name="remember">
                         <label class="form-check-label" for="remember">Remember me</label>
                     </div>
-                    <button type="submit" class="btn btn-primary btn-bg">Capture Fingerprint</button>
+                    <button type="submit" onclick="getFingerPrint()" class="btn btn-primary btn-bg">Capture Fingerprint</button>
                     {{-- <button type="submit" class="btn btn-primary">Login</button> --}}
                 </form>
             </div>
