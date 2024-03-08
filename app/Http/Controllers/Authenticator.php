46b8bb9319c17c0f50caefeb3e81918bf8aee6e6
cafeway <bcaranja@gmail.com>
2024-03-08 20:22:33 +0300

    In the controller i have added two index functions that return a view of the login and register pages respectively

--- app/Http/Controllers/Authenticator.php
+++ app/Http/Controllers/Authenticator.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class Authenticator extends Controller
 {
-    //
+    // this method renders the login page
+    public function LoginIndex(){ // Logins GET method handler
+        return view('login');
+    }
+
+
+// this method renders the register page
+    public function RegisterIndex() { // Register GET method handler
+        return view('register');
+    }
+
 }
