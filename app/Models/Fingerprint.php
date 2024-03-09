625d25f1759cdf28421d20e1fe1fb328f750d92a
cafeway <bcaranja@gmail.com>
2024-03-09 16:44:48 +0300

    i defined the fingerprint model and declared the availalbe fillables and a relationship to the user model

--- app/Models/Fingerprint.php
+++ app/Models/Fingerprint.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Fingerprint extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'fingerprint_data',
+    ];
+
+    // Define relationship with the User model if fingerprints are associated with users
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
