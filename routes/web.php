9a3a7c72b2b066739f01e927bbc1471b8b5057cc
cafeway <bcaranja@gmail.com>
2024-03-12 13:56:57 +0300

    added an auth middleware to the homepage route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
 Route::get('/login',[Authenticator::class, 'LoginIndex']);
 Route::get('/register',[Authenticator::class, 'RegisterIndex']);
